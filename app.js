260e981079751810f1966ddb6607d43265dd970e
Raymond Nazaryan <rayn2427@gmail.com>
2018-02-18 21:17:26 -0800

    Added multiple vue instances (app1 and app2)

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 new Vue({
-  el: '#app',
+  el: '#app1',
   data: {
     title: 'The VueJS Instance',
     showParagraph: false
@@ -22,5 +22,12 @@ new Vue({
     title: function(value) {
       alert('Title changed, new value: ' + value);
     }
+  }
+});
+
+new Vue({
+  el: '#app2',
+  data: {
+    title: 'The second Instance'
   }
 });
